3914fd1ae7ecdaf6d9cbb235fb324da761102df0
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-11-13 16:01:18 +1100

    Done with assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function focused(){
+        console.log('Good!')}
+    function blurred(){
+        console.log('Hey! Eyes on me!')}
+return (
+
+        <button onFocus={focused} onBlur={blurred}>Eyes on me</button>
+
+)
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function typing(){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        
+            <input type="password" onChange={typing} />
+        
     )
 }
 
